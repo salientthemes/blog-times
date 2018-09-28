ca66d610b33c88803f7895a172c346afe277f6fd
Nikesh Ulak <cloudnikesh@gmail.com>
2018-09-28 12:48:39 +0545

    Bootstrap loaded first before our style.

--- functions.php
+++ functions.php
@@ -9,6 +9,8 @@
 
   add_action( 'wp_enqueue_scripts', 'blog_times_style' );
   function blog_times_style() {
+  	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/assets/vendor/bootstrap/bootstrap.css' );
+  	
 	wp_enqueue_style( 'st-blog-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'blog-times-style',get_stylesheet_directory_uri() . '/style.css',array('st-blog-style'));
 }
